64688272d06f8f89d7ecb8bfe2d66a5172b99654
juankhusuma <juan.d.khusuma@gmail.com>
2025-02-15 23:26:34 +0700

    fix: added file server prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
+// @ts-check
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false
 };
 
-export default nextConfig;
+export default (phase) => {
+    const DEV = phase === PHASE_DEVELOPMENT_SERVER;
+    /** @type {import('next').NextConfig} */
+    const config = {
+        assetPrefix: DEV ? undefined : 'https://lexin.cs.ui.ac.id/chat/',
+    }
+
+    return { ...nextConfig, ...config }
+}
