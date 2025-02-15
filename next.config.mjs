139663bae6b9808b7e218b0a80a53c227aed1cd9
juankhusuma <juan.d.khusuma@gmail.com>
2025-02-16 01:14:19 +0700

    fix: changed base path to /chat/app

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,8 @@ export default (phase) => {
     const DEV = phase === PHASE_DEVELOPMENT_SERVER;
     /** @type {import('next').NextConfig} */
     const config = {
-        assetPrefix: DEV ? undefined : 'https://lexin.cs.ui.ac.id/chat/',
-        basePath: DEV ? undefined : '/chat',
+        assetPrefix: DEV ? undefined : 'https://lexin.cs.ui.ac.id/chat/app',
+        basePath: DEV ? undefined : '/chat/app',
     }
 
     return { ...nextConfig, ...config }
